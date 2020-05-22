2175be25a578bc2efa203f6c64b458147b55e649
Guillermoqbero <guillermoqbero@gmail.com>
2020-05-22 18:14:36 +0200

    chn

---
+++ src/Components/HolaMundo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function HolaMundo() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h2>Guillermo Cubero</h2>
+    </div>
+  );
+}
+
+export function AdiosMundo() {
+  return (
+    <div>
+      <h3>Adiós</h3>
+    </div>
+  );
+}
+
+/* export default HolaMundo; */
